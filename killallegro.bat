1a514273c4db2c5d9164524dfbcdf6e91fd98a5e
LongShaoShao <57096677+LongShaoShao@users.noreply.github.com>
2021-01-09 11:12:13 +0800

    创建killallegro.bat文件
    Allegro在使用过程中或在生成Arkwork文件时会产生大量临时备份文件，即使在Allegro内可以设置Arkwork文件的输出路径，但重复输出后依旧会产生大量的临时备份文件，本批处理文件旨在整理Arkwork文件的同时清理多余的临时备份文件。

---
+++ killallegro.bat
@@ -0,0 +1,41 @@
+::set CurDir=%~dp0
+::cd /d %CurDir%
+
+md Pdf
+::md Brd	                ::若想连Brd文件也处理，可将注释去掉，但不建议
+md Arkwork 
+md Arkwork\ASM
+md Arkwork\CAM
+md Arkwork\SMT
+move ADT.art Arkwork\ASM
+move ADB.art Arkwork\ASM
+move PASTBOT.art Arkwork\SMT
+move PASTTOP.art Arkwork\SMT
+move place_txt.txt Arkwork\SMT
+move *.art Arkwork\CAM
+move *.ipc Arkwork\CAM
+move *.drl Arkwork\CAM
+move *.rou Arkwork\CAM
+copy *.pdf Pdf
+copy ASM.pdf Arkwork\ASM
+copy SMT.pdf Arkwork\SMT
+::move *.brd Brd		 ::若想连Brd文件也处理，可将注释去掉，但不建议
+
+rd signoise.run /s 		/q
+del ".\*.log" 	/s 	/f 	/q
+del ".\*.log,*" /s 	/f 	/q
+del ".\*.art,*" 	/f 	/q
+del ".\*.jrl" 	/s	/f 	/q
+del ".\*.jrl,*" /s 	/f 	/q
+del ".\*.txt" 		/f 	/q
+del ".\*.txt,*" 	/f 	/q
+del ".\*.dml" 	/s 	/f 	/q
+del ".\*.iml" 	/s 	/f 	/q
+del ".\*.iml,*" /s 	/f 	/q
+del ".\*.ipc,*" 	/f 	/q
+del ".\*.rou,*" 	/f 	/q
+del ".\*.drl,*" 	/f 	/q
+del ".\*.lck"		/f 	/q
+del ".\*.pdf"		/f 	/q
+
+exit
